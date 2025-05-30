e74b4d99ccf569579fa6ea6ba66c3de05c718bc0
Eduardo A. Crespo <crespoed07@gmail.com>
2025-05-30 06:48:36 -0400

    Removed unised code.

--- client/src/App.js
+++ client/src/App.js
@@ -18,30 +18,3 @@ function App() {
 }
 
 export default App;
-
-
-// import React from 'react';
-// import { Routes, Route, Link } from 'react-router-dom';
-// import Home from './pages/Home';
-// import ExpenseTable from './pages/ExpenseTable';
-// import ExpenseDetail from './pages/ExpenseDetail';
-
-// function App() {
-//   return (
-//     <div>
-//       <nav style={{ padding: '1rem', backgroundColor: '#2A2A3C' }}>
-//         <Link to="/">Home</Link>
-//         <Link to="/expense-table">Expense Table</Link>
-//         <Link to="/expense/1">Expense Detail</Link>
-//       </nav>
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/expense-table" element={<ExpenseTable />} />
-//         <Route path="/expense/:id" element={<ExpenseDetail />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
